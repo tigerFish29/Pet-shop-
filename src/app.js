2d7f1d16cd80f5945e2403c8e08198063e3a8f28
tigerFish29 <paulwchabz28@gmail.com>
2022-08-05 01:42:57 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const Pet = (props) => {
+
+    // pet component 
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h3", {}, props.breed)
+    ]);
+};
+
+
+
+const App = () => {
+    return React.createElement(
+        "div", {},
+        [
+        React.createElement("h1", {}, "Adopt Me"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Sheep Dog"
+        })
+        ]
+    )
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
