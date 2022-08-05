456aac91198a3d68b46df75edb6671ebbe92d7eb
tigerFish29 <paulwchabz28@gmail.com>
2022-08-05 02:09:19 +0100

    initialised package json file

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 const Pet = (props) => {
-
-    // pet component 
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h3", {}, props.breed)
-    ]);
+  // pet component
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
-
-
 
 const App = () => {
-    return React.createElement(
-        "div", {},
-        [
-        React.createElement("h1", {}, "Adopt Me"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "Sheep Dog"
-        })
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Sheep Dog",
+    }),
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
